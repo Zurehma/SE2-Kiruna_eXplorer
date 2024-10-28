6d3251996a5ef07542018bc5acdf34747554f9a1
Murad <s329987@studenti.polito.it>
2024-10-28 16:48:02 +0100

    design of a home page

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import './styles/Home.css'
+import './styles/Home.css';
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="text-container">
-        <h1>Welcome to Our Office Management System</h1>
-        <p>Your efficient solution for managing office queues.</p>
+    <div className="home-background">
+      <div className="home-container">
+        <h1 className="home-title">"Kiruna: The Heart of Sweden's Iron Legacy and Gateway to the Arctic"</h1>
       </div>
-      <img
-        src="path/to/your/image.jpg" // Replace with your image path
-        alt="Animated visual"
-        className="animated-image"
-      />
     </div>
   );
 };
