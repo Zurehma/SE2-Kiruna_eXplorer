e1449dad9a1e2886d9c00c6f173ccff4e2c97de8
Luca Pes <s332943@studenti.polito.it>
2024-10-28 14:32:11 +0100

    added db and testdb. Added documentController, documentDAO and documentRoutes skeleton. Added API getDocument and addDocument design in API.md

---
+++ server/dao/documentoDAO.mjs
@@ -0,0 +1,17 @@
+import db from db;
+
+function DocumentDAO() {
+  this.getDocuments = () => {
+    return new Promise((resolve, reject) => {});
+  }
+
+  this.addDocument = () => {
+    return new Promise((resolve, reject) => {});
+  }
+
+  this.addLink = () => {
+    return new Promise((resolve, reject) => {});
+  }
+}
+
+export default DocumentDAO;
