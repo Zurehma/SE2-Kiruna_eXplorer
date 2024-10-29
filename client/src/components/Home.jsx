9125be9935d4dbd4cc459cd8f18b7bb42179a5da
Murad <s329987@studenti.polito.it>
2024-10-29 17:14:38 +0100

    homepage design

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import '../styles/Home.css'
+import '../styles/Home.css';
 import { Modal } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const [showModal, setShowModal] = useState(false);
   const [currentImage, setCurrentImage] = useState(null);
   const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 });
+  const navigate = useNavigate();
 
   const images = [
     {
@@ -46,36 +48,40 @@ const Home = () => {
     setCurrentImage(null);
   };
 
+  const handleButtonClick = () => {
+    navigate('/new-route'); // Replace '/new-route' with your actual route path
+  };
+
   return (
     <div className="home-background">
-        <div className="home-images">
+      <div className="home-images">
         {images.map((image) => (
-            <img
+          <img
             key={image.id}
             src={image.src}
             alt={`Image ${image.id}`}
-            className="static-image animated-image" 
+            className="static-image animated-image"
             style={{
-                ...image.style,
-                position: 'absolute',
-                cursor: 'pointer',
+              ...image.style,
+              position: 'absolute',
+              cursor: 'pointer',
             }}
             onClick={(event) => openModal(image, event)}
-            />
+          />
         ))}
-        </div>
+      </div>
 
       <div className="home-container">
-        <h1 className="home-title">"Kiruna: The Heart of Sweden's Iron Legacy and Gateway to the Arctic"</h1>
+        <h1 className="home-title">
+          "Kiruna: The Heart of Sweden's Iron Legacy and Gateway to the Arctic"
+        </h1>
+        <button className="redirect-button" onClick={handleButtonClick}>
+          Learn More
+        </button>
       </div>
 
       {currentImage && (
-        <Modal
-          show={showModal}
-          onHide={closeModal}
-          centered={false}
-          className="custom-modal"
-        >
+        <Modal show={showModal} onHide={closeModal} centered={false} className="custom-modal">
           <Modal.Body className="custom-modal-body" style={{ position: 'relative' }}>
             <h2 className="modal-description">{currentImage.description}</h2>
             <img src={currentImage.src} alt={`Image ${currentImage.id}`} className="modal-image" />
