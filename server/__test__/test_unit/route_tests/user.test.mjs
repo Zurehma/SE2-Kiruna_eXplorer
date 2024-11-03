94d15f2d46801484b8d9fabf7daa92e16cc93797
Zurehma Ayesha Rameez s323502 <s323502@studenti.polito.it>
2024-11-03 20:27:24 +0100

    User controller test

---
+++ server/__test__/test_unit/route_tests/user.test.mjs
@@ -0,0 +1,20 @@
+import { describe, test, expect, beforeAll, beforeEach, afterAll, afterEach, jest } from "@jest/globals";
+import UserController from "../../../controllers/userController.mjs";
+import User from "../../../models/user.mjs";
+import UserDao from "../../../dao/userDAO.mjs";
+import Utility from "../../../utility.mjs";
+import request from "supertest";
+
+const baseURL = "/api/sessions"
+
+jest.mock("../../../controllers/userController.mjs");
+
+describe("authRoutes", () => {
+
+    describe("POST /register", () => {
+        test("Create a new user", async () => {
+            expect(1).toBe(1);
+        });
+
+    });
+});
