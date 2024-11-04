43d5a86a2eec092e11a5f10856623c9800c99503
Murad <s329987@studenti.polito.it>
2024-11-04 22:56:40 +0100

    navbar color changed

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar, Nav, Dropdown, Button } from 'react-bootstrap';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
-import '../styles/Navbar.css';
+import '../styles/Navbar.css'; // Ensure custom styles are in this file
 
 export function NavigationBar(props) {
   const navigate = useNavigate();
@@ -18,21 +18,24 @@ export function NavigationBar(props) {
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ms-auto"> {/* Right-align everything */}
+        <Nav className="ms-auto">
           {props.role === 'Urban Planner' ? (
             <div className="d-flex align-items-center">
-              {/* Welcome text */}
               <i className="bi bi-person-circle text-white me-2"></i>
               <h5 className="text-white mt-1 welcome-text me-2">
                 Welcome, {props.username}
               </h5>
 
-              {/* Single Dropdown Menu for navigation actions */}
+              {/* Dropdown Menu */}
               <Dropdown align="end">
-                <Dropdown.Toggle variant="outline-white" id="dropdown-basic" className="btn-dark">
+                <Dropdown.Toggle
+                  // variant="outline-light"
+                  id="dropdown-basic"
+                  className="btn-dark menu-dropdown-toggle"
+                >
                   Menu
                 </Dropdown.Toggle>
-                <Dropdown.Menu className="dropdown-menu-end">
+                <Dropdown.Menu className="dropdown-menu-custom">
                   <Dropdown.Item onClick={() => { navigate('/map'); handleClose(); }}>
                     <i className="bi bi-map me-2"></i> Map
                   </Dropdown.Item>
@@ -55,10 +58,10 @@ export function NavigationBar(props) {
               </Dropdown>
             </div>
           ) : (
-            // If the user is not an Urban Planner, show login or home button
             location.pathname === '/login' ? (
               <Button
                 variant="outline-light"
+                id ='home-button'
                 className="ms-3"
                 onClick={() => { navigate('/'); handleClose(); }}
               >
@@ -67,7 +70,8 @@ export function NavigationBar(props) {
             ) : (
               <Button
                 variant="outline-light"
-                className="ms-3"
+                className="ms-3 custom-login-button"
+                id='login-button'
                 onClick={() => { navigate('/login'); handleClose(); }}
               >
                 Login
