bb869c4e0a7ba74c2b3167555d16b2dc4ac97b17
stefffffffffano <s323195@studenti.polito.it>
2024-11-24 12:30:16 +0100

    Design fix

--- client/src/components/Map2.jsx
+++ client/src/components/Map2.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'leaflet/dist/leaflet.css';
 
+
 import { MapContainer, TileLayer, Marker, Popup, Polygon, useMap, Polyline } from 'react-leaflet';
 import L from 'leaflet';
 
@@ -10,6 +11,8 @@ import { MyPopup } from './MyPopup';
 import API from '../../API';
 import { ShowDocuments } from './ShowDocuments';
 import { Dropdown } from 'react-bootstrap';
+//import kirunaCoordinates from '../assets/KirunaMunicipality.geojson';
+
 
 // Icon mapping based on document type
 const iconMap = {
@@ -35,13 +38,6 @@ const createCustomIcon = (type) => {
     });
 };
 
-// Custom icon for the "+" button
-const plusIcon = L.divIcon({
-    html: `<div style="display: flex; align-items: center; justify-content: center; background: white; width: 30px; height: 30px; border-radius: 50%; border: 2px solid black;">
-                <i class="bi bi-plus" style="color: black; font-size: 20px;"></i>
-           </div>`,
-    className: '' // Clear default class
-});
 
 // Custom hook to recenter the map
 const RecenterMap = ({ position, zoom }) => {
@@ -63,15 +59,13 @@ function Map2(props) {
     const [renderNumber,setRenderNumeber] = useState(0);
     const [typeDoc,setTypeDoc] = useState([]);
     const [selectedType, setSelectedType] = useState('All'); // New state for selected type
-    // Coordinate per il poligono (angoli specificati)-> to be loaded
-    const polygonCoordinates = [
-        [67.87328157366065, 20.20047943270466],
-        [67.84024426842895, 20.35839687019359],
-        [67.82082254726043, 20.181254701184297]
-    ];
+    
+    
 
-    // Sposta il bottone "+" leggermente più su del vertice superiore del triangolo
-    const plusButtonPosition = [67.8825492583, 20.2059690000253713];
+    // Trova il punto più alto nell'area di Kiruna (latitudine massima)
+    //const highestPoint = kirunaCoordinates.reduce((max, coord) => (coord[0] > max[0] ? coord : max), [-Infinity, -Infinity]);
+
+    
 
     useEffect(()=>{
         setLoading(true);
@@ -142,9 +136,11 @@ function Map2(props) {
     const coordDocuments = data.filter(doc => doc.lat != null && doc.long != null);
     /*
     Things to do:
-    Improve design of the dropdown when it's open
+    Improve placement of the doc-without-coordinates button- remove style in-line
     Import Kiruna coordinates and draw them when a noCoordinates element is opened
-    Increase z-index of the MyPopup component to make it stay over the dropdowns
+    Increase z-index of the MyPopup component to make it stay over the dropdowns->doesn't work
+    Give the possibility of multiple views in the map (e.g. satellite, terrain, etc.)
+    <Polygon positions={kirunaCoordinates} color="blue" fillOpacity={0.3} />
     */
     return (
         <>
@@ -159,7 +155,6 @@ function Map2(props) {
                 {/* Filter dropdown on the top-right corrner: to be fixed */}
                 {props.loggedIn && <Dropdown onSelect={(eventKey) => setSelectedType(eventKey)} className='myDropdownFilter'>
                     <Dropdown.Toggle variant="light" id="dropdown-filter-button" className='myFilterMenu'>
-                        <i className="bi bi-filter me-1 myMapIcons"></i>
                         Filter
                     </Dropdown.Toggle>
                     <Dropdown.Menu style={{ backgroundColor: 'white' }}>
@@ -174,25 +169,34 @@ function Map2(props) {
                 }
                 {/*Show documents without coordinates with a button that groups them all*/}
                 <Dropdown onSelect={(eventKey) => {setSelectedDoc(noCoordDocuments.at((doc)=>doc.id===eventKey)); setRenderNumeber((renderNumber) => renderNumber + 1);}} className='myDropdownDocuments'>
-                    <Dropdown.Toggle variant="light" id="dropdown-DocumentWithoutCoordinates-button" className='myFilterMenu'>
-                        Kiruna municipality
+                    <Dropdown.Toggle variant="light" id="dropdown-DocumentWithoutCoordinates-button" className='myFilterMenu text-small'>
+                        Municipality
                     </Dropdown.Toggle>
-                    <Dropdown.Menu style={{ backgroundColor: 'white' }}>
+                    <Dropdown.Menu style={{ 
+            backgroundColor: 'white', 
+            maxHeight: '200px', 
+            overflowY: 'scroll', 
+            flexDirection: 'column',
+        }}>
                         {noCoordDocuments.map((doc) => (
-                            <Dropdown.Item key={doc.id} eventKey={doc.id}>
-                                <i className={`bi ${iconMap[doc.type]}`} style={{ fontSize: '20px', color: '#555' }}/>
-                                <p className=' ms-3 small'>{doc.title}</p>
+                            <Dropdown.Item className='ms-1 me-1 mb-1' key={doc.id} eventKey={doc.id} style={{display: 'flex',justifyContent: 'center', alignItems: 'center', border: '1px solid black',padding: '5px', // Riduce lo spazio interno
+                                width: '150px',  // Imposta una larghezza fissa più stretta
+                                height: '30px',}}>
+                                <i className={`bi ${iconMap[doc.type]}`} style={{ fontSize: '16px', color: 'black' }} />
                             </Dropdown.Item>
                         ))}
                     </Dropdown.Menu>
                 </Dropdown>
                 {/* Render coordinate documents as markers on the map */}
                 {<ShowDocuments data={coordDocuments} createCustomIcon={createCustomIcon} setSelectedDoc={setSelectedDoc} setRenderNumeber={setRenderNumeber} renderNumber={renderNumber} />}
-
+                {/* Renderizza il poligono di Kiruna se un documento senza coordinate è selezionato */}
+                
+                
+                
                 {/* If a document is selected, show MyPopup in a popup */}
                 {selectedDoc && (
                     (() => {
-                        const pos = selectedDoc.lat ? [selectedDoc.lat, selectedDoc.long] : plusButtonPosition;
+                        const pos = selectedDoc.lat ? [selectedDoc.lat, selectedDoc.long] : highestPoint;
                         const myKey = selectedDoc.id+renderNumber;
                         return (
                             <Popup position={pos} maxWidth={800} key={myKey} onClose={() => setSelectedDoc(null)}>
