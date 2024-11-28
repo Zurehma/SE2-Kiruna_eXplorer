871bff0dbd97e5d35670804e1beba28978282110
Gianluca880600 <s273910@studenti.polito.it>
2024-11-28 12:03:14 +0100

    new division for addDoc

---
+++ client/src/components/Doc/Step1.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { Card, Form, Row, Col } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+
+const Step1 = ({
+  document,
+  errors,
+  handleChange,
+  currentStakeholders,
+  isAddingNew,
+  newStakeholder,
+  handleNewStakeholderChange,
+  handleNewBlur,
+  handleSelectChange,
+  stakeholders,
+}) => (
+  <Card className="mb-4">
+    <Card.Body>
+      {/* TITLE */}
+      <Row className="mb-3">
+        <Form.Group controlId="title">
+          <Form.Label>Title*</Form.Label>
+          <Form.Control
+            type="text"
+            name="title"
+            value={document.title}
+            onChange={handleChange}
+            placeholder="Enter document title"
+            className="input"
+            isInvalid={!!errors.title}
+          />
+          <Form.Control.Feedback type="invalid">{errors.title}</Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      {/* STAKEHOLDER */}
+      <Row className="mb-3">
+        <Col className="mb-3" md={6}>
+          <Form.Group controlId="stakeholder">
+            <Form.Label>Stakeholders*</Form.Label>
+            <Form.Select
+              as="select"
+              name="stakeholder"
+              value={document.stakeholder || ""}
+              onChange={handleSelectChange}
+              className="input-multi"
+              isInvalid={!!errors.stakeholder}
+            >
+              <option value="">Select a stakeholder</option>
+              {currentStakeholders.map((stakeholder, index) => (
+                <option key={index} value={stakeholder.name}>
+                  {stakeholder.name}
+                </option>
+              ))}
+              <option value="add_new">+ Add new stakeholder</option>
+            </Form.Select>
+            {errors.stakeholder && (
+              <Form.Control.Feedback type="invalid">{errors.stakeholder}</Form.Control.Feedback>
+            )}
+          </Form.Group>
+        </Col>
+        {isAddingNew && (
+          <Col md={6}>
+            <Form.Group controlId="newStakeholder">
+              <Form.Label>New Stakeholder</Form.Label>
+              <Form.Control
+                type="text"
+                name="newStakeholder"
+                value={newStakeholder}
+                onChange={handleNewStakeholderChange}
+                onBlur={handleNewBlur} // Salva automaticamente al perdere il focus
+                placeholder="Enter new stakeholder"
+                className="input"
+              />
+            </Form.Group>
+          </Col>
+        )}
+      </Row>
+
+      {/* DESCRIPTION */}
+      <Row className="mb-3">
+        <Form.Group controlId="description" className="mb-4">
+          <Form.Label>Description*</Form.Label>
+          <Form.Control
+            as="textarea"
+            name="description"
+            minLength={2}
+            value={document.description || ""}
+            onChange={handleChange}
+            placeholder="Enter a brief description"
+            className="input-textarea"
+            isInvalid={!!errors.description}
+          />
+          <Form.Control.Feedback type="invalid">{errors.description}</Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+    </Card.Body>
+  </Card>
+);
+
+export default Step1;
