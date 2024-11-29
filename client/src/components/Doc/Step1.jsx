a8166bad877737f1789630e66890aee3a7a1ef5b
Gianluca880600 <s273910@studenti.polito.it>
2024-11-29 17:51:31 +0100

    addLink new version

--- client/src/components/Doc/Step1.jsx
+++ client/src/components/Doc/Step1.jsx
@@ -54,50 +54,6 @@ const Step1 = ({
         </Col>
       </Row>
 
-      {/* STAKEHOLDER */}
-      {/* <Row className="mb-3">
-        <Col className="mb-3" md={6}>
-          <Form.Group controlId="stakeholder">
-            <Form.Label>Stakeholders*</Form.Label>
-            <Form.Select
-              as="select"
-              name="stakeholder"
-              value={document.stakeholder || ""}
-              onChange={handleSelectChange}
-              className="input-multi"
-              isInvalid={!!errors.stakeholder}
-            >
-              <option value="">Select a stakeholder</option>
-              {currentStakeholders.map((stakeholder, index) => (
-                <option key={index} value={stakeholder.name}>
-                  {stakeholder.name}
-                </option>
-              ))}
-              <option value="add_new">+ Add new stakeholder</option>
-            </Form.Select>
-            {errors.stakeholder && (
-              <Form.Control.Feedback type="invalid">{errors.stakeholder}</Form.Control.Feedback>
-            )}
-          </Form.Group>
-        </Col>
-        {isAddingNew && (
-          <Col md={6}>
-            <Form.Group controlId="newStakeholder">
-              <Form.Label>New Stakeholder</Form.Label>
-              <Form.Control
-                type="text"
-                name="newStakeholder"
-                value={newStakeholder}
-                onChange={handleNewStakeholderChange}
-                onBlur={handleNewBlur} // Salva automaticamente al perdere il focus
-                placeholder="Enter new stakeholder"
-                className="input"
-              />
-            </Form.Group>
-          </Col>
-        )}
-      </Row> */}
-
       {/* DESCRIPTION */}
       <Row className="mb-3">
         <Form.Group controlId="description" className="mb-4">
