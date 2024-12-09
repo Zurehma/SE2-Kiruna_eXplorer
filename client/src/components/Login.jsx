df629bbaa2b4db52321ec4f83cedf4946076116b
Gianluca880600 <s273910@studenti.polito.it>
2024-12-09 18:24:00 +0100

    new login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,72 +1,64 @@
-import React, { useEffect } from 'react';
-import { Alert, Button, Col, Form, Row } from 'react-bootstrap';
-import '../styles/Login.css';
+import React from "react";
+import SlidingPane from "react-sliding-pane";
+import "react-sliding-pane/dist/react-sliding-pane.css";
+import "../styles/Login.css";
+import { Form, Button } from "react-bootstrap";
 
-function Login({ handleLogin, username, password, setUsername, setPassword, loggedinError, setloggedinError }) {
-    
-    useEffect(() => {
-        // Set a timer to clear loggedinError after 5 seconds if there is an error
-        if (loggedinError) {
-            const timer = setTimeout(() => {
-                setloggedinError(null);
-            }, 5000);
+const Login = ({
+  isLoginOpen,
+  closeLoginPane,
+  username,
+  password,
+  setUsername,
+  setPassword,
+  handleLogin,
+}) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const credentials = { username, password };
+    await handleLogin(credentials);
+    closeLoginPane();
+  };
 
-            // Clear the timer if loggedinError changes or if the component unmounts
-            return () => clearTimeout(timer);
-        }
-    }, [loggedinError]);
+  return (
+    <SlidingPane
+      className="login-pane"
+      isOpen={isLoginOpen}
+      title="Login"
+      from="right"
+      width="400px"
+      onRequestClose={closeLoginPane}
+    >
+      <Form className="login-form" onSubmit={handleSubmit}>
+        <h2 className="login-title">Login</h2>
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const credentials = { username, password };
-        await handleLogin(credentials);
-    };
+        <Form.Group className="form-group" controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            placeholder="Example: admin"
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            placeholder="Enter your password."
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            minLength={6}
+          />
+        </Form.Group>
+        <Button className="login-button" type="submit">
+          Login
+        </Button>
+      </Form>
+    </SlidingPane>
+  );
+};
 
-    return (
-        <div className="login-background">
-            <Row className="justify-content-md-center">
-                <Col md={12} className="login-container">
-                    <h1 className="pb-3">Login</h1>
-
-                    {/* Static space for the alert */}
-                    <div className="alert-placeholder">
-                        {loggedinError && (
-                            <Alert variant="danger" dismissible onClose={() => setloggedinError(null)}>
-                                {loggedinError}
-                            </Alert>
-                        )}
-                    </div>
-
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group className="mb-2" controlId="username">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control
-                                type="text"
-                                value={username}
-                                placeholder="Example: admin"
-                                onChange={(ev) => setUsername(ev.target.value)}
-                                required
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                value={password}
-                                placeholder="Enter your password."
-                                onChange={(ev) => setPassword(ev.target.value)}
-                                required
-                                minLength={6}
-                            />
-                        </Form.Group>
-                        <Button className="mt-3 btn-black" type="submit">
-                            Login
-                        </Button>
-                    </Form>
-                </Col>
-            </Row>
-        </div>
-    );
-}
-
-export { Login };
+export default Login;
