153b5da921c5c303a009c2753299387dd988e211
stefffffffffano <s323195@studenti.polito.it>
2024-12-17 09:55:03 +0100

    Solved bug in document List

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -5,6 +5,40 @@ import "react-datepicker/dist/react-datepicker.css";
 import { format } from "date-fns";
 import API from "../../../API";
 
+function updateDocument(doc){
+  if (!doc.coordinates || doc.coordinates.length === 0) {
+    return { ...doc, lat: null, long: null };
+  } else {
+    if (doc.coordinates.length > 1) {
+      // Function to find the highest point in a polygon, where the popup and the marker will be shown
+      const highestPoint = doc.coordinates.reduce(
+        (max, [lat, long]) => {
+          return lat > max.lat ? { lat, long } : max;
+        },
+        { lat: doc.coordinates[0][0], long: doc.coordinates[0][1] }
+      );
+
+      const area = doc.coordinates.map(([lat, long]) => [lat, long]);
+
+      return {
+        ...doc,
+        lat: highestPoint.lat,
+        long: highestPoint.long,
+        area,
+      };
+    } else {
+      // Single coordinate
+      const { lat, long } = doc.coordinates;
+      return {
+        ...doc,
+        lat,
+        long,
+      };
+    }
+  }
+}
+
+
 function Filters(props) {
   const { showDateFilters = true } = props;
   const [stakeholder, setStakeholder] = useState(
@@ -57,7 +91,7 @@ function Filters(props) {
           ? props.currentPage
           : undefined;
       const all = props.currentPage !== null && props.currentPage !== undefined ? false : true;
-      console.log("Current Page: ", currentPage);
+      
       const paginatedFilters = { ...filteredParams, pageNo: currentPage + 1 };
       let response = await API.getDocuments(paginatedFilters, all);
 
@@ -67,8 +101,10 @@ function Filters(props) {
           props.setCurrentPage(0);
         }
         props.setTotalPages(response.totalPages);
+        response.elements = response.elements.map(updateDocument);
         props.setDocuments(response.elements);
       } else {
+        response = response.map(updateDocument);
         props.setDocuments(response);
       }
     } catch (error) {
